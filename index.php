d64eda2c015fa1dc6b3b102a4fa72a279451c17d
Martin Drzola <zola@zdnb.(none)>
2011-11-29 12:06:06 +0100

    design changes

--- index.php
+++ index.php
@@ -1,59 +1,48 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <?php
-    if (isset($_POST['date']))
-        $date = strtotime($_POST['date']);
-    else
-        $date = strtotime("now");
+if (isset($_POST['date']))
+    $date = strtotime($_POST['date']);
+else
+    $date = strtotime("now");
 ?>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <link rel="stylesheet" href="css/styles.css" type="text/css" />
         <link type="text/css" href="css/ui-lightness/jquery-ui-1.8.16.custom.css" rel="stylesheet" />
-        <title>Prototyp</title>
-        <script type="text/javascript" src="js/jquery.js"></script>
-        <script type="text/javascript" src="js/popup.js"></script>
-        <script type="text/javascript" src="js/popup_window.js"></script>
-        <script type="text/javascript" src="js/jquery-ui-1.8.16.custom.min.js"></script>
-        <script type="text/javascript" src="js/jquery.ui.datepicker-de.js"></script>
-        <script>
-            $(function() {
-                $.datepicker.setDefaults( $.datepicker.regional[ "" ] );
-                $( "#datepicker" ).datepicker( $.datepicker.regional[ "de" ] );
-                $( "#locale" ).change(function() {
-                    $( "#datepicker" ).datepicker( "option",
-                    $.datepicker.regional[ $( this ).val() ] );
-                });
-            });
-        </script>
+        <title>quellvital - Termine</title>
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
     </head>
     <body>
         <center>
             <div id="container">
-                <div id="header"><h2>Terminverwaltung</h2>Logout</div>
+                <div id="header"><h2>Terminverwaltung</h2></div>
                 <div id="topnav">
-                    <center>
+                    <div id="leftnav">
                         <a href="#" id="current"><img src="css/calendar.png" />Kalender</a>
-                        <a href="#"><img src="css/list.png" />Terminliste</a>
-                        <a href="#"><img src="css/calendar_add.png" />Termin erstellen</a>
-                        <a href="#"><img src="css/group.png" />Mitarbeiterverwaltung</a>
+                        <a href="liste.html"><img src="css/list.png" />Suche</a>
                         <a href="#"><img src="css/chart_bar.png" />Statistiken</a>
-                    </center>
+                        <a href="#"><img src="css/date_add.png" />Termin erstellen</a>
+                        <a href="mitarbeiter.html"><img src="css/group.png" />Mitarbeiter</a>
+                    </div>
+                    <div id="rightnav">
+                        <a href="">Logout</a>
+                    </div>
                 </div>
                 <div id="nav">
                     <table>
                         <tr>
                             <td style="width:100px;text-align:left;">
                                 <form action="index.php" method="POST">
-                                    <input name="date" type="text" hidden="yes" value="<?php echo date('d.m.Y', $date - 604800); ?>" />
+                                    <input name="date" type="hidden" value="<?php echo date('d.m.Y', $date - 604800); ?>" />
                                     <input type="image" src="css/prev.png" title="vorherige Woche" alt="submit" />
                                 </form>
                             </td>
                             <td style="width:600px;text-align:center;">
                                 <form action="index.php" method="POST">
                                     <select name="user">
-                                        <option>Heinz</option>
-                                        <option>Paul</option>
+                                        <option>oliverm</option>
+                                        <option>bernhardb</option>
                                     </select>
                                     <input id="datepicker" type="text" name="date" value="<?php echo date('d.m.Y', strtotime("now")); ?>" />
                                     <input type="submit" value="Los" />
@@ -61,7 +50,7 @@
                             </td>
                             <td style="width:100px;text-align:right;">
                                 <form action="index.php" method="POST">
-                                    <input name="date" type="text" hidden="yes" value="<?php echo date('d.m.Y', $date + 604800); ?>" />
+                                    <input name="date" type="hidden" value="<?php echo date('d.m.Y', $date + 604800); ?>" />
                                     <input type="image" src="css/next.png" title="nächste Woche" alt="submit" />
                                 </form>
                             </td>
@@ -70,6 +59,21 @@
                 </div>
                 <div id="content">
                     <div id="kalcontainer">
+                        <script type="text/javascript" src="js/jquery.js"></script>
+                        <script type="text/javascript" src="js/popup.js"></script>
+                        <script type="text/javascript" src="js/popup_window.js"></script>
+                        <script type="text/javascript" src="js/jquery-ui-1.8.16.custom.min.js"></script>
+                        <script type="text/javascript" src="js/jquery.ui.datepicker-de.js"></script>
+                        <script>
+                            $(function() {
+                                $.datepicker.setDefaults( $.datepicker.regional[ "" ] );
+                                $( "#datepicker" ).datepicker( $.datepicker.regional[ "de" ] );
+                                $( "#locale" ).change(function() {
+                                    $( "#datepicker" ).datepicker( "option",
+                                    $.datepicker.regional[ $( this ).val() ] );
+                                });
+                            });
+                        </script>
                         <ul id="zeit">
                             <li>8:00</li>
                             <li>9:00</li>
--- login.php
+++ login.php
@@ -1,41 +1,42 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-		<link rel="stylesheet" href="css/login.css" type="text/css">
-		<title>QV Login</title>
-	</head>
-	<body>
-		<div id="container">
-		<?php
-				$u_agent = $_SERVER['HTTP_USER_AGENT'];
-				if(preg_match('/MSIE 8./i',$u_agent) || preg_match('/MSIE 7./i',$u_agent) || preg_match('/MSIE 6./i',$u_agent)) { ?>
-					<p>
-						<b>Ihr Browser wird nicht unterstützt!</b>
-						Bitte laden und installieren Sie einen standardkonformen Browser, um dieses Produkt effektiv nutzen zu können:
-							</br>
-							</br><a href='http://www.mozilla.org/firefox'>Mozilla Firefox</a>
-							</br><a href='http://www.google.com/chrome'>Google Chrome</a>
-							</br><a href='http://www.opera.com/'>Opera</a>
-					</p>
-			<?php } ?>
-			<h3>Terminverwaltung - Login</h3>
-			<form method="POST" action="checklogin.php">
-				<table>
-					<tr>
-						<td>Username:</td>
-						<td><input type="text" name="user" /></td>
-					</tr>
-					<tr>
-						<td>Passwort:</td>
-						<td><input type="password" name="password" /></td>
-					</tr>
-					<tr>
-						<td></td>
-						<td><input type="submit" value="Login" /></td>
-					</tr>
-				</table>		
-			</form>
-		</div>
-	</body>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <link rel="stylesheet" href="css/login.css" type="text/css" />
+        <title>QV Login</title>
+    </head>
+    <body>
+        <div id="container">
+            <?php
+            $u_agent = $_SERVER['HTTP_USER_AGENT'];
+            if (preg_match('/MSIE 8./i', $u_agent) || preg_match('/MSIE 7./i', $u_agent) || preg_match('/MSIE 6./i', $u_agent)) {
+                ?>
+                <p>
+                    <b>Ihr Browser wird nicht unterstützt!</b>
+                    Bitte laden und installieren Sie einen standardkonformen Browser, um dieses Produkt effektiv nutzen zu können:
+                    </ br>
+                    </ br><a href='http://www.mozilla.org/firefox'>Mozilla Firefox</a>
+                    </ br><a href='http://www.google.com/chrome'>Google Chrome</a>
+                    </ br><a href='http://www.opera.com/'>Opera</a>
+                </p>
+            <?php } ?>
+            <h3>Terminverwaltung - Login</h3>
+            <form method="POST" action="index.php">
+                <table>
+                    <tr>
+                        <td>Username:</td>
+                        <td><input type="text" name="user" value="user"/></td>
+                    </tr>
+                    <tr>
+                        <td>Passwort:</td>
+                        <td><input type="password" name="password" value="asdfafa" /></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><input type="submit" value="Login" /></td>
+                    </tr>
+                </table>		
+            </form>
+        </div>
+    </body>
 </html>
