db451b7f808d276756fc6bc6a3ebc0c1cdf298c2
Martin Drzola <zola@zdnb.(none)>
2012-01-19 15:11:25 +0100

    datenbank

--- qv_datenbank.sql
+++ qv_datenbank.sql
@@ -1,54 +1,137 @@
-CREATE TABLE IF NOT EXISTS termin(
-	terminID int(16) NOT NULL AUTO_INCREMENT,
-	mitarbeiterID Varchar(50) NOT NULL,
-	kundenID Int(16),
-        terminart Char(1) NOT NULL,
-	termindatum Timestamp NOT NULL,
-	termindauer Int(5) NOT NULL,
-	quelle Int(16),
-	abschlussart Int(16),
-	bemerkung VARCHAR(1000),
-	PRIMARY KEY(terminID)); 
-	
-CREATE TABLE IF NOT EXISTS kunde(
-	kundenID Int(16) PRIMARY KEY AUTO_INCREMENT,
-	anrede VARCHAR(10) NOT NULL,
-	titel VARCHAR(20),
-	vorname VARCHAR(50) NOT NULL,
-	nachname VARCHAR(50) NOT NULL,
-	strasse VARCHAR(100) NOT NULL,
-	ort VARCHAR(100) NOT NULL,
-	plz VARCHAR(15) NOT NULL,
-	mobil Varchar(20),
-	festnetz Varchar(20),
-	beruf Varchar(50),
-	bemerkung VARCHAR(1000));
-	
-CREATE TABLE IF NOT EXISTS kaufvertrag(
-	vertragID Int(16) PRIMARY KEY AUTO_INCREMENT,
-	zahlungsart Int(16) NOT NULL);
+-- phpMyAdmin SQL Dump
+-- version 3.3.7deb6
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Erstellungszeit: 19. Januar 2012 um 15:10
+-- Server Version: 5.1.49
+-- PHP-Version: 5.3.3-7+squeeze3
 
-CREATE TABLE IF NOT EXISTS user(
-	username Varchar(50) PRIMARY KEY,
-	vorname VARCHAR(50) NOT NULL,
-	nachname VARCHAR(50) NOT NULL,
-	pwd Char(32) NOT NULL,
-	access_level Char(1) NOT NULL);
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
-CREATE TABLE IF NOT EXISTS quelle(
-	quellenID Int(16) PRIMARY KEY,
-	quellenbezeichnung VARCHAR(100) NOT NULL);
 
-CREATE TABLE IF NOT EXISTS abschlussart(
-	abschlussartID Int(16) PRIMARY KEY,
-	abschlussartbezeichung VARCHAR(100)NOT NULL);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
-CREATE TABLE IF NOT EXISTS zahlungsart(
-	zahlungsartID Int(1) PRIMARY KEY,
-	zahlungsartbezeichung VARCHAR(100)NOT NULL);
-	
-CREATE TABLE IF NOT EXISTS produkt(
-	produktID Int(2) PRIMARY KEY,
-	produktname Varchar(50) NOT NULL,
-	preis Int(10) NOT NULL);
+--
+-- Datenbank: `qv`
+--
 
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `abschlussart`
+--
+
+CREATE TABLE IF NOT EXISTS `abschlussart` (
+  `abschlussartID` int(16) NOT NULL,
+  `abschlussartbezeichung` varchar(100) NOT NULL,
+  PRIMARY KEY (`abschlussartID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `kaufvertrag`
+--
+
+CREATE TABLE IF NOT EXISTS `kaufvertrag` (
+  `vertragID` int(16) NOT NULL AUTO_INCREMENT,
+  `zahlungsart` int(16) NOT NULL,
+  PRIMARY KEY (`vertragID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `kunde`
+--
+
+CREATE TABLE IF NOT EXISTS `kunde` (
+  `kundenID` int(16) NOT NULL AUTO_INCREMENT,
+  `anrede` varchar(10) NOT NULL,
+  `titel` varchar(20) DEFAULT NULL,
+  `vorname` varchar(50) NOT NULL,
+  `nachname` varchar(50) NOT NULL,
+  `strasse` varchar(100) NOT NULL,
+  `ort` varchar(100) NOT NULL,
+  `plz` varchar(15) NOT NULL,
+  `mobil` varchar(20) DEFAULT NULL,
+  `festnetz` varchar(20) DEFAULT NULL,
+  `beruf` varchar(50) DEFAULT NULL,
+  `bemerkung` varchar(1000) DEFAULT NULL,
+  PRIMARY KEY (`kundenID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `produkt`
+--
+
+CREATE TABLE IF NOT EXISTS `produkt` (
+  `produktID` int(2) NOT NULL,
+  `produktname` varchar(50) NOT NULL,
+  `preis` int(10) NOT NULL,
+  PRIMARY KEY (`produktID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `quelle`
+--
+
+CREATE TABLE IF NOT EXISTS `quelle` (
+  `quellenID` int(16) NOT NULL,
+  `quellenbezeichnung` varchar(100) NOT NULL,
+  PRIMARY KEY (`quellenID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `termin`
+--
+
+CREATE TABLE IF NOT EXISTS `termin` (
+  `terminID` int(16) NOT NULL AUTO_INCREMENT,
+  `mitarbeiterID` varchar(50) NOT NULL,
+  `kundenID` int(16) NOT NULL,
+  `terminart` char(1) NOT NULL,
+  `termindatum` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `termindauer` int(5) NOT NULL,
+  `quelle` int(16) DEFAULT NULL,
+  `abschlussart` int(16) DEFAULT NULL,
+  `bemerkung` varchar(1000) DEFAULT NULL,
+  PRIMARY KEY (`terminID`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `username` varchar(50) NOT NULL,
+  `vorname` varchar(50) NOT NULL,
+  `nachname` varchar(50) NOT NULL,
+  `password` char(32) NOT NULL,
+  `access_level` char(1) NOT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `zahlungsart`
+--
+
+CREATE TABLE IF NOT EXISTS `zahlungsart` (
+  `zahlungsartID` int(1) NOT NULL,
+  `zahlungsartbezeichung` varchar(100) NOT NULL,
+  PRIMARY KEY (`zahlungsartID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
